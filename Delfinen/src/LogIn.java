2f573982bc76c55a098cb7e2f8588fe3275769da
Kevin Strandberg <kevin@npstrandberg.comm>
2023-11-21 14:52:25 +0100

    .

--- Delfinen/src/LogIn.java
+++ Delfinen/src/LogIn.java
@@ -1,3 +1,48 @@
-public class LogIn {
-    
+package security;
+
+public class Login {
+
+    public Login {
+        this.main = main;
+        password = new Password();
+    }
+
+    public void loginAdmin() {
+        while (main.isRunning()) {
+            (...)
+        }
+    }
+
+    public void loginAdmin() {
+        UI.println("Enter password");
+        UI.promptString();
+        password.checkPassword(UI.promptString(), "drowningkids");
+        while (password.isPasswordCorrect() && main.isRunning()) {
+ showMenu.showHarryMenu();
+        }
+        if (!password.isPasswordCorrect()) {
+            main.setRunning(false);
+        }
+    }
+
+    public void loginCash(Main.main) {
+        UI.println("Enter password");
+        UI.promptString();
+        password.checkPassword(UI.promptString(), "oldmandrowned");
+        while (password.isPasswordCorrect() && main.isRunning()) {
+        }
+        if (!password.isPasswordCorrect()) {
+            main.setRunning(false);
+
+    public void loginCoach() {
+         UI.println("Enter password");
+         UI.promptString();
+         password.checkPassword(UI.promptString(), "fuck");
+         while (password.isPasswordCorrect() && main.isRunning()) {
+                 showMenu.showAccountantMenu();
+                }
+         if (!password.isPasswordCorrect()) {
+             main.setRunning(false);
+        }
+    }
 }
